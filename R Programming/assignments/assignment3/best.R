74360b59800b481ac0181be3740f66f67eb96bc0
sp1r0s <sp1r0s@users.noreply.github.com>
2015-03-25 21:16:03 +0000

    Made some changes

--- R Programming/assignments/assignment3/best.R
+++ R Programming/assignments/assignment3/best.R
@@ -1,16 +1,31 @@
 best <- function(state, outcome) {
-  ## Read outcome data
-  outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-  
-  ## Check that state and outcome are valid
-  if (!is.character(state) || length(state) > 2) {
-    stop("invalid state")
-  }
-  
-  if (!is.character(outcome)) {
-      stop("invalid outcome");
-  }
-  
-  ## Return hospital name in that state with lowest 30-day death rate
-  
+    ## Read outcome data
+    outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
+    ## Check that state is valid
+    if (!is.character(state) || nchar(state) > 2) {
+        stop("invalid state")
+    }
+    
+    # Check that outcome is valid
+    if (!is.character(outcome)
+        && (outcome != "heart attack" || outcome != "heart failure" || outcome != "pneumonia")) {
+        stop("invalid outcome");
+    }
+    
+    
+    
+    ## Return hospital name in that state with lowest 30-day death rate
+    stateData <- subset(outcomeData, State == state)
+    lowestRate <- NULL
+    
+    if (outcome == "heart attack") {
+        lowestRate <- min(stateData[, 11])  
+    } else if (outcome == "heart failure") {
+        lowestRate <- min(stateData[, 17])  
+    } else if (outcome == "pneumonia") {
+        lowestRate <- min(stateData[, 23])  
+    }
+    
+    return(subset(stateData, 'Hospital 30-Day Death (Mortality) Rates from Heart Attack' == lowestRate))
 }
