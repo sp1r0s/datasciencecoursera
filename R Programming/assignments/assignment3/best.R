95c5f27f5f52cb66944c8f92c3948b7bc65dd9d9
sp1r0s <sp1r0s@users.noreply.github.com>
2015-03-26 21:38:01 +0000

    Finished with best.R

--- R Programming/assignments/assignment3/best.R
+++ R Programming/assignments/assignment3/best.R
@@ -1,31 +1,38 @@
 best <- function(state, outcome) {
-    ## Read outcome data
-    outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
-    
-    ## Check that state is valid
-    if (!is.character(state) || nchar(state) > 2) {
-        stop("invalid state")
-    }
-    
-    # Check that outcome is valid
-    if (!is.character(outcome)
-        && (outcome != "heart attack" || outcome != "heart failure" || outcome != "pneumonia")) {
-        stop("invalid outcome");
-    }
-    
-    
-    
-    ## Return hospital name in that state with lowest 30-day death rate
-    stateData <- subset(outcomeData, State == state)
-    lowestRate <- NULL
-    
-    if (outcome == "heart attack") {
-        lowestRate <- min(stateData[, 11])  
-    } else if (outcome == "heart failure") {
-        lowestRate <- min(stateData[, 17])  
-    } else if (outcome == "pneumonia") {
-        lowestRate <- min(stateData[, 23])  
-    }
-    
-    return(subset(stateData, 'Hospital 30-Day Death (Mortality) Rates from Heart Attack' == lowestRate))
+
+  outcomes <- c("heart attack", "heart failure", "pneumonia")
+  
+  ## Check if outcome is valid
+  if (!outcome %in% outcomes) {
+    stop("invalid outcome")
+  }
+  
+  ## Find the right column
+  outcomeColumn <- NULL
+  if (outcome == "heart attack") {
+    outcomeColumn <- 11  
+  } else if (outcome == "heart failure") {
+    outcomeColumn <- 17
+  } else if (outcome == "pneumonia") {
+    outcomeColumn <- 23
+  }
+  
+  ## Read data
+  outcomeData <- read.csv("outcome-of-care-measures.csv", colClasses = "character")[,c(2,7,outcomeColumn)]
+  
+  ## Check if state is valid
+  if (!state %in% outcomeData$State) {
+    stop("invalid state")
+  }
+  
+  ## Get a subset for the state we want
+  outcomeData = subset(outcomeData, State == state & outcomeData[3] != "Not Available")
+  
+  ## Change the outcome type to numeric for better results
+  outcomeData[, 3] = as.numeric(outcomeData[, 3])
+  
+  ## Return hospital name in that state with lowest 30-sday death rate
+  outcomeData = outcomeData[order(outcomeData[, 3], outcomeData$Hospital.Name), ]
+  return(outcomeData$Hospital.Name[1])
+  
 }
